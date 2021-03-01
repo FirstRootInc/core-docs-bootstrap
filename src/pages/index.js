d3cc09856c211bbdf92871ca5261bb04ce741e3f
Sudhi Damodhar <sudhidamodhar@gmail.com>
2021-03-02 03:29:26 +0530

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,7 @@ import styles from './styles.module.css';
 
 const features = [
   {
-    title: 'Easy to Use',
+    title: 'Developer Tool',
     imageUrl: 'img/undraw_docusaurus_mountain.svg',
     description: (
       <>
@@ -18,7 +18,7 @@ const features = [
     ),
   },
   {
-    title: 'Focus on What Matters',
+    title: 'Security',
     imageUrl: 'img/undraw_docusaurus_tree.svg',
     description: (
       <>
@@ -28,7 +28,7 @@ const features = [
     ),
   },
   {
-    title: 'Powered by React',
+    title: 'Account',
     imageUrl: 'img/undraw_docusaurus_react.svg',
     description: (
       <>
