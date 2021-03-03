49cae07630161adf3197e22db4a7946d50496480
julielhote <julie.lhote@firstroot.co>
2021-03-03 17:09:31 +0100

    Deploy website - based on 4da2c2c84655ccfb379f8a27411aec4ec03f3e1e

--- 8494b983.7a0dfd07.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[17],{89:function(o){o.exports=JSON.parse('{"title":"Recent posts","items":[{"title":"Welcome","permalink":"/core-docs-bootstrap/blog/welcome"},{"title":"Hello","permalink":"/core-docs-bootstrap/blog/hello-world"},{"title":"Hola","permalink":"/core-docs-bootstrap/blog/hola"}]}')}}]);
